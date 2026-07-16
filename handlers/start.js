00600c2f63a4f138a2439173be0524bcb9bb8bab
NoorAlHidayaAIBot <thamer.thamer0099@gmail.com>
2026-07-16 22:24:25 +0300

    Update start.js

--- handlers/start.js
+++ handlers/start.js
@@ -1,25 +1,13 @@
 const bot = require("../config/bot");
 const keyboard = require("../keyboards/mainMenu");
+const { welcomeMessage } = require("../utils/messages");
 
 bot.onText(/^\/start$/, (msg) => {
-  const chatId = msg.chat.id;
-
-  const welcomeMessage = `السلام عليكم ورحمة الله وبركاته 🌿
-
-أهلاً بك في نور الهداية.
-
-نسأل الله أن يبارك في الهادي وفاطمة، وأن يرزقهما الصحة والعافية والبركة، وأن يجعل هذا العمل خالصًا لوجهه الكريم.
-
-اللهم صل وسلم وبارك على سيدنا محمد ﷺ.
-
-يمكنك سؤالي عن:
-
-📖 تفسير القرآن الكريم.
-📚 الأحاديث النبوية الصحيحة.
-⚖️ الفقه الإسلامي مع بيان أقوال المذاهب الأربعة.
-🌍 يمكنك الكتابة بأي لغة، وسأجيبك باللغة نفسها بإذن الله.`;
-
-  bot.sendMessage(chatId, welcomeMessage, keyboard);
+  bot.sendMessage(
+    msg.chat.id,
+    welcomeMessage,
+    keyboard
+  );
 });
 
 module.exports = bot;
