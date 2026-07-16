5202165697353e7c41de0783bc82d0401c4ca63d
NoorAlHidayaAIBot <thamer.thamer0099@gmail.com>
2026-07-16 16:27:14 +0300

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -1,1 +1,8 @@
-.
+const TelegramBot = require("node-telegram-bot-api");
+require("dotenv").config();
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, {
+  polling: true,
+});
+
+module.exports = bot;
