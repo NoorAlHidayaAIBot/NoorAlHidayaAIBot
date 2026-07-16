c46cccc1a8306110975b42e2ad616ec8d8c6e53b
NoorAlHidayaAIBot <thamer.thamer0099@gmail.com>
2026-07-16 16:28:17 +0300

    Update mainMenu.js

--- keyboards/mainMenu.js
+++ keyboards/mainMenu.js
@@ -1,1 +1,13 @@
-.
+const keyboard = {
+  reply_markup: {
+    keyboard: [
+      ["📖 تفسير القرآن", "📚 الأحاديث"],
+      ["⚖️ الفقه الإسلامي", "🕌 الأدعية والأذكار"],
+      ["🌍 تغيير اللغة", "ℹ️ عن البوت"]
+    ],
+    resize_keyboard: true,
+    persistent: true
+  }
+};
+
+module.exports = keyboard;
