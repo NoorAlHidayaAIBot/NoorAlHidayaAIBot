89e3fa75933724ce00172c750f89bf1f769f4b09
NoorAlHidayaAIBot <thamer.thamer0099@gmail.com>
2026-07-18 00:36:55 +0300

    Update menu.js

--- handlers/menu.js
+++ handlers/menu.js
@@ -1,5 +1,10 @@
 const bot = require("../config/bot");
 
+require("./tafsir");
 require("./hadith");
+require("./fiqh");
+require("./adhkar");
+require("./language");
+require("./about");
 
 module.exports = bot;
